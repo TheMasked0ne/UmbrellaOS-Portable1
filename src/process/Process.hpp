1b833afe7128ef3ea1365d3681e737ec2017c762
TheMaskedOne <122543688+TheMasked0ne@users.noreply.github.com>
2025-04-10 11:41:17 -0700

    Create Process.hpp
    all will be as ver 0.1

---
+++ src/process/Process.hpp
@@ -0,0 +1,35 @@
+#pragma once
+#include <string>
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+#include <thread>
+#include <atomic>
+
+struct Message {
+    int source;
+    int destination;
+    std::string data;
+};
+
+class Process {
+protected:
+    int id;
+    std::string name;
+    std::queue<Message> messageQueue;
+    std::mutex queueMutex;
+    std::condition_variable queueCV;
+    std::atomic<bool> running{true};
+    std::thread workerThread;
+
+    virtual void handleMessage(const Message& msg);
+    void processLoop();
+
+public:
+    Process(int pid, const std::string& pname);
+    virtual ~Process();
+
+    int getId() const;
+    void sendMessage(const Message& msg);
+    void stop();
+};
